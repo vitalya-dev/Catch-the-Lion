978194ee310c4e0bfeeb0101e38628718c7be169
Vitalya Dev <vitalya.dev@gmail.com>
2023-09-24 19:08:40 +0300

    Some refactoring.

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -4,23 +4,27 @@ class_name Main
 
 var selected_piece = null
 
+@onready var board = $Board
+@onready var captured_pieces_area_1 = $CapturedPieces/Player1
+@onready var captured_pieces_area_2 = $CapturedPieces/Player2
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Board.board_clicked.connect(_on_board_clicked)
+	board.board_clicked.connect(_on_board_clicked)
 	for piece in get_node("Board/Pieces").get_children():
 		piece.piece_clicked.connect(_on_piece_clicked)
 
 func _on_board_clicked(grid_pos):
 	if selected_piece:
-		var piece_pos = $Board.get_piece_grid_position(selected_piece)
+		var piece_pos = board.get_piece_grid_position(selected_piece)
 		var move = grid_pos - piece_pos
 		if move in selected_piece.get_possible_moves():
-			var destination_piece = $Board.get_piece_at_grid_position(grid_pos)
+			var destination_piece = board.get_piece_at_grid_position(grid_pos)
 			if destination_piece:
 				if destination_piece.player == selected_piece.player:
 					print("Illegal move. Grid position already occupied by our player")
 				else:
-					var captured_pieces_area = get_node("CapturedPieces/" + str(selected_piece.player))
+					var captured_pieces_area = captured_pieces_area_1 if selected_piece.player == 1 else captured_pieces_area_2
 					captured_pieces_area.add_piece(destination_piece)
 					move_selected_piece_to_grid_position(grid_pos)
 					_on_piece_clicked(selected_piece)
@@ -29,7 +33,7 @@ func _on_board_clicked(grid_pos):
 				_on_piece_clicked(selected_piece)
 
 func move_selected_piece_to_grid_position(grid_pos):
-	var global_pos = $Board.grid_to_global(grid_pos)
+	var global_pos = board.grid_to_global(grid_pos)
 	selected_piece.global_transform.origin.x = global_pos.x
 	selected_piece.global_transform.origin.z = global_pos.z
 	selected_piece.transform.origin.y = 0
